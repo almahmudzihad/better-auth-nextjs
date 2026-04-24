6cfa57e10a6c99fa4b2a693a256fc62386ba5ce5
almahmudzihad <amzihad@gmail.com>
2026-04-24 07:11:12 +0600

    better auth step 8

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,6 +6,9 @@ const db = client.db();
 
 
 export const auth = betterAuth({
+    emailAndPassword: { 
+    enabled: true, 
+  },
      database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
